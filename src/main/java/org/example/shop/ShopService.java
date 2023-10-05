4ed23703dd8596de326bb751dcba2899141944ea
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-05 15:57:26 +0200

    Refactor code in ShopService class

--- src/main/java/org/example/shop/ShopService.java
+++ src/main/java/org/example/shop/ShopService.java
@@ -4,17 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShopService {
+    OrderRepo orderRepo;
 
-    OrderRepo orderListRepo;
-    OrderRepo orderMapRepo;
-
-    public ShopService(OrderListRepo orderListRepo) {
-        this.orderListRepo = orderListRepo;
+    public ShopService(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
     }
-    public ShopService(OrderMapRepo orderMapRepo) {
-        this.orderMapRepo = orderMapRepo;
-    }
-
 
     public void makeOrder(
             ProductRepo allProducts,
@@ -31,12 +25,7 @@ public class ShopService {
 
         if(containsValidProducts) {
             Order order = new Order(orderNumber, customerName,orderedProducts);
-            if(orderListRepo != null) {
-                this.orderListRepo.addOrder(order);
-            } else {
-                this.orderMapRepo.addOrder(order);
-            }
-
+            this.orderRepo.addOrder(order);
         } else {
             System.out.println("Order contains invalid product");
         }
